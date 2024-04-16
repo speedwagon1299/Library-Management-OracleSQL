ba5d825be904a408d9c22f6d52f024dcf5ce24a3
Shreya Sinha <118337644+cloudparent@users.noreply.github.com>
2024-04-17 03:55:22 +0530

    Add files via upload

---
+++ src/ChildrenBookDetailsController.java
@@ -0,0 +1,42 @@
+package ChildrenBookDetails;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
+
+public class ChildrenBookDetailsController {
+    
+}
+
+
+ 
+
+    @FXML
+    private TextField a_id_tf;
+
+    @FXML
+    private TextField a_name_tf;
+
+    @FXML
+    private Button cancel_btn;
+
+    @FXML
+    private Label response_lb;
+
+    @FXML
+    private Button save_btn;
+
+    @FXML
+    void CancelAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SaveCB(ActionEvent event) {
+
+    }
+
+}
